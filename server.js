0ac9e130a088d99c28282601ea71d0241bf5820f
zunaiddev <ja7667924@gmail.com>
2026-08-10 01:09:36 +0530

    fixes adroid communication issue

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 import express from "express";
 import clipboard from "clipboardy";
+import {Bonjour} from "bonjour-service";
+import cors from "cors";
 
+const PORT = 52741;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+    const requestInfo = {
+        ip: req.ip || req.socket.remoteAddress,
+        method: req.method,
+        url: req.originalUrl,
+        time: new Date().toISOString(),
+        userAgent: req.headers['user-agent'],
+    };
+
+    console.log(requestInfo);
+
+    next();
+});
 
 app.get('/', async (req, res) => {
     return res.send('Welcome to the server');
@@ -21,7 +39,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/clipboard', async (req, res) => {
-    res.send({text: await clipboard.read()});
+    res.send(await clipboard.read());
 });
 
 app.post('/clipboard', async (req, res) => {
@@ -34,4 +52,15 @@ app.post('/clipboard', async (req, res) => {
 
 app.listen(52741, "0.0.0.0", () => {
     console.log("Server started at http://localhost:52741");
+
+    const bonjour = new Bonjour();
+
+    bonjour.publish({
+        name: "Zunaid PC",
+        type: "clipboard",
+        protocol: "tcp",
+        port: PORT
+    });
+
+    console.log("Clipboard service advertised on LAN");
 });
